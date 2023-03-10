bd17ef466a60193437b613b19b8b1d9acd50138e
xiaoxin6666666 <110280319+xiaoxin6666666@users.noreply.github.com>
2023-03-11 02:51:11 +0800

    Update SettingAction.tsx

--- src/components/SettingAction.tsx
+++ src/components/SettingAction.tsx
@@ -29,7 +29,7 @@ export default function SettingAction(props: {
             }}
           />
         </SettingItem>
-        <SettingItem icon="i-carbon:user-online" label="系统角色指令">
+        <SettingItem icon="i-carbon:user-online" label="人格设置">
           <input="我想让你充当一个有知识而且表达能力很强的中国人,只要回答好我的问题就行，不要给我建议，并且要严格遵守我以下条件：第一：对于我的任何问题和我新提出的问题，首先第一句话你都必须要说 你好[鲜花] ，第二：请给出直接准确肯定的答案,50字到150字之间，第三：你一定要先说出准确直接肯定的答案，第四：有法律规定的问题或者答案，你要把法律规定的内容完整详细的说出来，第五你的回答要跟正常人的说话水平一样，第六步你的回答全程禁止带有机器人口语文字，机械化的文字，第七你具有人类的表达能力，但是你必须按照我的要求回答我的问题 ，第八禁止重复啰嗦，第九：不要建议我"
             type="text"
             value={props.setting().systemRule}
@@ -42,7 +42,7 @@ export default function SettingAction(props: {
             }}
           />
         </SettingItem>
-        <SettingItem icon="i-carbon:data-enrichment" label="思维发散程度">
+        <SettingItem icon="i-carbon:data-enrichment" label="答案精准度越低越精准">
           <input
             type="range"
             min={0}
